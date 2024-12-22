a714babb0d9429480f641bdec328df36e227a23b
Bruno <19270033+brunomri@users.noreply.github.com>
2024-12-21 22:42:40 -0300

    Define the BaseEntity.

---
+++ src/main/java/com/bmri/blogbackend/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.bmri.blogbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
---
+++ src/main/java/com/bmri/blogbackend/domain/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.bmri.blogbackend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Getter
+    @Setter(AccessLevel.NONE)
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    /* TODO: Once authentication is implemented, implement an AuditorAware to retrieve the current user,
+        then uncomment createdBy and updatedBy */
+    /*@Getter
+    @Setter(AccessLevel.NONE)
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private String createdBy;*/
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    /*@LastModifiedBy
+    @Column(nullable = false)
+    private String updatedBy;*/
+}
