e9d25fbb855feceee9e8bd085495004d8849cbe9
Brunomri <19270033+Brunomri@users.noreply.github.com>
2024-12-22 18:44:28 -0300

    Create a post repository.

---
+++ src/main/java/com/bmri/blogbackend/repositories/PostRepository.java
@@ -0,0 +1,20 @@
+package com.bmri.blogbackend.repositories;
+
+import com.bmri.blogbackend.domain.PostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostEntity, Long> {
+    Page<PostEntity> getByPublishedIsTrue(Pageable pageable);
+
+    Optional<PostEntity> getByTitle(String title);
+
+    Page<PostEntity> getByCategory(String category, Pageable pageable);
+
+    Page<PostEntity> getByTagsContaining(String tag, Pageable pageable);
+}
