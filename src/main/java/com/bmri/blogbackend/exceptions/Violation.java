f7bc1074cec71624af87d37e2531c28bab6759fc
Brunomri <19270033+Brunomri@users.noreply.github.com>
2024-12-24 17:47:48 -0300

    Create Violation and ValidationErrorResponse to store information when request constraints are violated and ConstraintViolationException is throws.

---
+++ src/main/java/com/bmri/blogbackend/exceptions/Violation.java
@@ -0,0 +1,19 @@
+package com.bmri.blogbackend.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class Violation extends StandardErrorResponse {
+
+    private final String objectName;
+    private final String fieldName;
+    private final Object value;
+
+    public Violation(String timestamp, Integer status, String exception, String objectName, String fieldName,
+                     Object value, String message) {
+        super(timestamp, status, exception, message);
+        this.objectName = objectName;
+        this.fieldName = fieldName;
+        this.value = value;
+    }
+}
