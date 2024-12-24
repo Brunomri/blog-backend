78cd46c0f87bcdfda2da47bff0b732e84e78db1b
Bruno <19270033+brunomri@users.noreply.github.com>
2024-12-24 17:54:53 -0300

    Create a custom ObjectNotFoundException to throw when a post is not found. Instead of returning null from getPostById, this exception will be thrown and then GlobalExceptionHandler will handle it to return 404.

---
+++ src/main/java/com/bmri/blogbackend/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,18 @@
+package com.bmri.blogbackend.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
