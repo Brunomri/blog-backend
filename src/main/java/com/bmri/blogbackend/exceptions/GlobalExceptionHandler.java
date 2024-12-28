88d88da8d05debdf1101de12912eb0dd8fb05483
Brunomri <19270033+Brunomri@users.noreply.github.com>
2024-12-28 16:08:21 -0300

    Add endpoint to exceptions response and remove unnecessary fields.

--- src/main/java/com/bmri/blogbackend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bmri/blogbackend/exceptions/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -23,13 +24,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    ValidationErrorResponse handleConstraintViolationException(ConstraintViolationException e) {
+    ValidationErrorResponse handleConstraintViolationException(ConstraintViolationException e, WebRequest request) {
         var error = new ValidationErrorResponse();
         for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
             error.getViolations().add(
                     new Violation(formatter.format(LocalDateTime.now()), HttpStatus.BAD_REQUEST.value(),
-                            violation.getClass().getName(), violation.getRootBeanClass().getName(),
-                            violation.getPropertyPath().toString(), violation.getInvalidValue(), violation.getMessage())
+                            request.getDescription(false), violation.getPropertyPath().toString(),
+                            violation.getInvalidValue(), violation.getMessage())
             );
         }
         return error;
@@ -38,13 +39,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    ValidationErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    ValidationErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e, WebRequest request) {
         var error = new ValidationErrorResponse();
         for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
             error.getViolations().add(
                     new Violation(formatter.format(LocalDateTime.now()), HttpStatus.BAD_REQUEST.value(),
-                            fieldError.getClass().getName(), fieldError.getObjectName(),
-                            fieldError.getField(), fieldError.getRejectedValue(), fieldError.getDefaultMessage())
+                            request.getDescription(false), fieldError.getField(),
+                            fieldError.getRejectedValue(), fieldError.getDefaultMessage())
             );
         }
         return error;
@@ -53,33 +54,33 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    StandardErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    StandardErrorResponse handleHttpMessageNotReadableException(HttpMessageNotReadableException e, WebRequest request) {
         return new StandardErrorResponse(formatter.format(LocalDateTime.now()), HttpStatus.BAD_REQUEST.value(),
-                e.getClass().getName(), e.getMessage());
+                request.getDescription(false), e.getMessage());
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     @ResponseBody
-    StandardErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+    StandardErrorResponse handleDataIntegrityViolationException(DataIntegrityViolationException e, WebRequest request) {
         return new StandardErrorResponse(formatter.format(LocalDateTime.now()), HttpStatus.CONFLICT.value(),
-                e.getClass().getName(), e.getMessage());
+                request.getDescription(false), "Database constraint violated");
     }
 
     @ExceptionHandler(ObjectNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ResponseBody
-    StandardErrorResponse handleObjectNotFoundException(ObjectNotFoundException e) {
+    StandardErrorResponse handleObjectNotFoundException(ObjectNotFoundException e, WebRequest request) {
         return new StandardErrorResponse(formatter.format(LocalDateTime.now()), HttpStatus.NOT_FOUND.value(),
-                e.getClass().getName(), e.getMessage());
+                request.getDescription(false), e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
-    StandardErrorResponse handleGeneralException(Exception e) {
+    StandardErrorResponse handleGeneralException(Exception e, WebRequest request) {
         return new StandardErrorResponse(formatter.format(LocalDateTime.now()), HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                e.getClass().getName(), e.getMessage());
+                request.getDescription(false), "Internal server error");
     }
 
 }
--- src/main/java/com/bmri/blogbackend/exceptions/StandardErrorResponse.java
+++ src/main/java/com/bmri/blogbackend/exceptions/StandardErrorResponse.java
@@ -9,6 +9,6 @@ public class StandardErrorResponse {
 
     private final String timestamp;
     private final Integer status;
-    private final String exception;
+    private final String detail;
     private final String message;
 }
